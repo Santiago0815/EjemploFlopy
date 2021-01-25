8e39b79dc32ea012e2a99f5f6cadf39c5e1251b4
santiago serrano <santiago2180196@correo.uis.edu.co>
2021-01-25 17:25:23 -0500

    Ahora con datos principales

--- Ejem1.py
+++ Ejem1.py
@@ -3,6 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import flopy
 # -*- coding: utf-8 -*-
+name = "tutorial1"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
 """
 Created on Mon Jan 25 15:35:07 2021
 
