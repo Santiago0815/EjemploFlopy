69900117e234d0e11f9dd8dbeed748707ade0ebd
santiago serrano <santiago2180196@correo.uis.edu.co>
2021-01-29 12:51:15 -0500

    terminado

--- Ejem1.py
+++ Ejem1.py
@@ -90,6 +90,33 @@ sim.write_simulation()
 success, buff = sim.run_simulation()
 if not success:
     raise Exception("MODFLOW 6 did not terminate normally.")
+ #condicion de exito   
+  
+headfile= 'workspace' + '/' + headfile  
+hds = flopy.utils.binaryfile.HeadFile(headfile)
+h = hds.get_data(kstpkper=(0, 0))
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[0], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%2.1f")    
+#imprimiendo resultados
+
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[-1], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
+#ploteando una capa distinta
+
+z = np.linspace(-H / Nlay / 2, -H + H / Nlay / 2, Nlay)
+fig = plt.figure(figsize=(5, 2.5))
+ax = fig.add_subplot(1, 1, 1, aspect="auto")
+c = ax.contour(x, z, h[:, 50, :], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
+#ploteando una seccion transversal
 """
 Created on Mon Jan 25 15:35:07 2021
 
