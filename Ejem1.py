9b8d9e55461a9cb6518c7403db65fdbe3390cb70
santiago serrano <santiago2180196@correo.uis.edu.co>
2021-01-29 11:16:52 -0500

    agregar comandos sim, tdis, ims, gwf

--- Ejem1.py
+++ Ejem1.py
@@ -13,8 +13,21 @@ H = 50.0
 k = 1.0
 
 sim = flopy.mf6.MFSimulation(
-    sim_name=name, exe_name="mf6", version="mf6", sim_ws="."
+    sim_name=name, exe_name="C:/Users/PERSONAL/Diplomado/mf6", version="mf6", sim_ws="workspace"
 )
+#busca mf6 en la carpeta especificada y guarda los archivos
+
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)]
+)
+#crear objetos de flopy TDIS
+
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+#crear el paquete de objetos de flopy IMS
+
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
+#crea el modelo de flujo de agua
 
 """
 Created on Mon Jan 25 15:35:07 2021
